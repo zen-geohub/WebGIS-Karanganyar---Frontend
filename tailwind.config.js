7b756bb76ee621eed60fbfa97dd7059c6c4588dd
zen-geohub <m008bsy0791@bangkit.academy>
2024-04-09 16:24:02 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./public/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))',
+      },
+      gridTemplateRows: {
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))'
+      },
+      gridRow: {
+        'span-12': 'span 12 / span 12',
+        'span-13': 'span 13 / span 13'
+      },
+      gridColumn: {
+        'span-13': 'span 13 / span 13',
+        'span-14': 'span 14 / span 14'
+      },
+      gridColumnStart: {
+        '14': '14',
+        '15': '15',
+      },
+      gridColumnEnd: {
+        '14': '14',
+        '15': '15',
+        '16': '16',
+      },
+    },
+  },
+  plugins: [],
+}
+
